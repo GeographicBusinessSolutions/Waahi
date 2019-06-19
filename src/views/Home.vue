815dcdbb19bc8a7b24feabd34eee297846077e8f
David Aalbers <d.aalbers@outlook.com>
2019-06-19 15:20:33 +1200

    Recreated docs site. uses existing docx (Word) files saved as HTML. Edits can be saved to /public/docs.

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+        <img
+            alt="Vue logo"
+            src="../assets/logo.png"
+        >
+        
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import "bulma";
+
+export default {
+    name: "home",
+    components: {}
+};
+</script>
